4ab0f2365fec2de2b681b5e3ac988d88696eefbb
magnusrodseth <magnus.rodseth@gmail.com>
2024-06-02 22:50:31 +0200

    add neotree and dashboard to nvim config

--- .config/nvim/lua/kickstart/plugins/dashboard.lua
+++ .config/nvim/lua/kickstart/plugins/dashboard.lua
@@ -2,7 +2,36 @@ return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
-    require('dashboard').setup {}
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          {
+            desc = 'Update',
+            action = 'Lazy update',
+            key = 'u',
+          },
+          {
+            desc = 'Files',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = 'Grep',
+            action = 'Telescope live_grep',
+            key = 'g',
+          },
+          {
+            desc = 'Explorer',
+            action = 'Neotree toggle',
+            key = 'e',
+          },
+        },
+      },
+    }
   end,
   dependencies = { { 'nvim-tree/nvim-web-devicons' } },
 }
