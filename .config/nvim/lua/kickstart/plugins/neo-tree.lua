9de7f0137deae559e928db8fdff2dae5fb3d5f2d
magnusrodseth <magnus.rodseth@gmail.com>
2024-06-16 13:20:29 +0200

    include hidden items in telescope and neo-tree

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -24,6 +24,11 @@ return {
         enabled = true, -- This will find and focus the file in the active buffer every time
         leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
       },
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
     },
     window = {
       position = 'right',
