41cb7eb01beb105b88aca689aa7da6bd40eea2b9
magnusrodseth <magnus.rodseth@gmail.com>
2024-08-13 14:17:45 +0200

    update caps to esc

--- scripts/macos/map-esc-to-caps.sh
+++ scripts/macos/map-esc-to-caps.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-# Add symlink for KeyRemapping
-ln -s "$HOME/dotfiles/macos/com.local.KeyRemapping.plist" "$HOME/Library/LaunchAgents/com.local.KeyRemapping.plist" 
+FILENAME="com.local.KeyRemapping"
+
+# Check if the com.local.KeyRemapping.plist file exists
+if [ ! -f "$HOME/Library/LaunchAgents/$FILENAME.plist" ]; then
+  echo "Error: The KeyRemapping configuration file does not exist."
+  exit 1
+fi
 
 # Make sure the config is unloaded first
-launchctl unload "$HOME/Library/LaunchAgents/com.local.KeyRemapping.plist"
+launchctl unload "$HOME/Library/LaunchAgents/$FILENAME.plist"
 
 # Then load the config
-launchctl load "$HOME/Library/LaunchAgents/com.local.KeyRemapping.plist"
+launchctl load "$HOME/Library/LaunchAgents/$FILENAME.plist"
 
 # Check that the config was loaded
-if ! launchctl list | grep -q "com.local.KeyRemapping"; then
-    echo "Error: The KeyRemapping configuration was not loaded."
-    exit 1
+if ! launchctl list | grep "$FILENAME"; then
+  echo "Error: The KeyRemapping configuration was not loaded."
+  exit 1
 else
-    echo "KeyRemapping configuration loaded successfully."
+  echo "KeyRemapping configuration loaded successfully."
 fi
 
 # Start the remapping on system startup
-launchctl start com.local.KeyRemapping.plist
+launchctl start "$FILENAME.plist"
