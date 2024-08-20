c9776211366de608f9f30a090b71374b6f609458
magnusrodseth <magnus.rodseth@gmail.com>
2024-08-20 11:11:16 +0200

    update keymaps to support navigation using 1-9

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
+
+-- Loop to create keymaps for switching buffers using <leader>1 to <leader>9 and <leader>0 with :BufferLineGoToBuffer
+for i = 1, 9 do
+  vim.keymap.set("n", "<leader>" .. i, "<cmd>BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to Buffer " .. i })
+end
+
+-- Map <leader>0 to the 10th buffer
+vim.keymap.set("n", "<leader>0", "<cmd>BufferLineGoToBuffer 10<CR>", { desc = "Go to Buffer 10" })
