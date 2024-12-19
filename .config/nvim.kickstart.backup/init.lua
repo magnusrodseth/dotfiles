a9e87f26e2afdd6d977d1ee67f06a36b747aa5b1
magnusrodseth <magnus.rodseth@gmail.com>
2024-12-19 11:07:01 +0100

    update nvim config

--- .config/nvim.kickstart.backup/init.lua
+++
@@ -1,7 +0,0 @@
-require 'kickstart.basic'
-require 'kickstart.keymaps'
-require 'kickstart.autocommands'
-require 'kickstart.plugins.lazy'
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
