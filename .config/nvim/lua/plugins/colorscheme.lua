d347f98ba6e8e9707b07063555ded4508cc00b2c
magnusrodseth <magnus.rodseth@gmail.com>
2024-12-19 11:07:01 +0100

    update nvim config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  "folke/tokyonight.nvim",
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "tokyonight",
     },
-  }
+  },
 }
